bb61656e5f24ac4cc887fd2544e0e3a0250f2aff
unknown <l144261@lhr.nu.edu.pk>
2019-07-02 17:10:03 +0500

    basic fabricjs working

--- src/js/exp1.js
+++ src/js/exp1.js
@@ -1,96 +1,64 @@
-
+import 'fabric/dist/fabric.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import { inherits } from 'util';
+import { NONAME } from 'dns';
 
 
 
-$('#can').click(function(){  
-    var c = new $("canvas");
-
-   var ctx = c[0].getContext('2d');
-  ctx.beginPath();
-   ctx.arc(20,30,10,0,2*Math.PI);
-   
-   ctx.moveTo(90,30);
-   
-   ctx.arc(80,30,10,0,2*Math.PI);
-   ctx.moveTo(40,70);
-   
-   ctx.arc(30,70,10,0,2*Math.PI);
-   ctx.stroke();
-   var i;
-   for (i = 0; i < c.length; i++) {
-   c[i].addEventListener("click", color);
-   }
-   function color(){
-    
-    ctx.fill();
-   }
-    
-
+ 
+ // create a wrapper around native canvas element (with id="c")
+ $('#can').click(function(){ 
+  $(document).ready(function init(){
+ 
+    canvas.setHeight(500);
+    canvas.setWidth(800);
+    $('canvas').bind("click",obj.test);
   });
+ var canvas = new fabric.Canvas('canvas');
+// create a rectangle object
+var c1;
+var c2;
+class Circle1{
+  s(l,t,c,f,wi,ra){
+    var circle  = new fabric.Circle({
+      left: l,
+      top: t,
+      stroke:c,
+      fill:f,
+      
+      strokeWidth:wi,
+      radius:ra,});
+      canvas.add(circle);
+      
+      return circle;
+  }
+  test(){
+    
+    c2.set('fill','red');
+    console.log("ali");
 
-  //moving a circle
-  var mouseX = 0;
-var mouseY = 0;
-  function setMousePosition(e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-  }    
-  function getPosition(el) {
-    var xPosition = 0;
-    var yPosition = 0;
-   
-    while (el) {
-      xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
-      el = el.offsetParent;
-    }
-    return {
-      x: xPosition,
-      y: yPosition
-    };
+c1.set('fill','blue');
+canvas.add();
+
   }
-  var canvasPos = getPosition(canvas);
-var mouseX = 0;
-var mouseY = 0;
- 
-canvas.addEventListener("mousemove", setMousePosition, false);
- 
-function setMousePosition(e) {
-  mouseX = e.clientX - canvasPos.x;
-  mouseY = e.clientY - canvasPos.y;
 }
-function update() {
-    context.beginPath();
-    context.arc(mouseX, mouseY, 50, 0, 2 * Math.PI, true);
-    context.fillStyle = "#FF6A6A";
-    context.fill();
-   
-    requestAnimationFrame(update);
-  }
-  update();
-  function update() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-   
-    context.beginPath();
-    context.arc(mouseX, mouseY, 50, 0, 2 * Math.PI, true);
-    context.fillStyle = "#FF6A6A";
-    context.fill();
-   
-    requestAnimationFrame(update);
-  }    
-  canvas.addEventListener("mousemove", setMousePosition, false);
+ let obj=new Circle1();
+  c2=obj.s(200,200,'black','green',3,20);
+  c1=obj.s(200,100,'black','green',2,20);
  
-function setMousePosition(e) {
-  mouseX = e.clientX - canvasPos.x;
-  mouseY = e.clientY - canvasPos.y;
  
-  context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+
  
-  context.beginPath();
-  context.arc(mouseX, mouseY, 50, 0, 2 * Math.PI, true);
-  context.fillStyle = "#FF6A6A";
-  context.fill();
-}
+
+
+ });
+
+
+
+
+ 
+
